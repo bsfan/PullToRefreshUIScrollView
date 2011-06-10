2221de920ded64f184d03768e6769275ee284b05
Josh <jmgrenon@gmail.com>
2011-06-10 01:30:53 -0500

    just some code cleanup

--- Classes/PullToRefreshScrollViewController.m
+++ Classes/PullToRefreshScrollViewController.m
@@ -25,14 +25,6 @@
 }
 */
 
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView {
-}
-*/
-
-
-
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
